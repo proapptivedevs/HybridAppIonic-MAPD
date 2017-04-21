78bebd2130c474e562fdd0e04c52d4579141ce32
Luis Esquivel <laesquivel@hotmail.com>
2017-04-21 11:45:53 -0400

    fetching data from firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  mapd: FirebaseListObservable<any[]>;
+
+  constructor(public navCtrl: NavController,
+              public af: AngularFire) {
+
+                this.mapd = af.database.list('/mapd/');
 
   }
 
