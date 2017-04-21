6fcba3351a0d406b50ae25b5cd7955469ffefda0
Luis Esquivel <laesquivel@hotmail.com>
2017-04-21 19:30:01 -0400

    lasta changes

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+ semester1: FirebaseListObservable<any[]>;
+  semester2: FirebaseListObservable<any[]>;
+  
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public af: AngularFire) {
 
-  constructor(public navCtrl: NavController) {
+                this.semester1 = af.database.list('/faculty/semester1');
+                this.semester2 = af.database.list('/faculty/semester2');
+
 
   }
 
