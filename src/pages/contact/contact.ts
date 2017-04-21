bb4b7951cd8e3e05014c81d90c1e50544a6b05c2
Luis Esquivel <laesquivel@hotmail.com>
2017-04-21 18:59:20 -0400

    adding comments

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  comments: FirebaseListObservable<any[]>;
+
+  private commentForm : FormGroup;
+
+  constructor(public navCtrl: NavController,
+              af: AngularFire,
+              private formBuilder: FormBuilder ) {
+
+                this.comments = af.database.list('/comments');
+                this.commentForm = this.formBuilder.group({
+                      name: ['', Validators.required],
+                      comment: [''],
+                      email: [''],
+                      date: Date(),
+                });
+              }
+
+    newComment(){
+
+      this.comments.push( this.commentForm.value );
+    
+    }
 
   }
 
-}
+
