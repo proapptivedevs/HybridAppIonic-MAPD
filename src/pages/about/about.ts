f72f003817295a6d278c33b3a7e4ad01055c5ae2
jaismeensandhu <jaismeen.sandhu@yahoo.com>
2017-04-21 23:15:15 -0400

    Added Search option

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import firebase from 'firebase';
 
 @Component({
@@ -8,30 +7,25 @@ import firebase from 'firebase';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  semester1: FirebaseListObservable<any[]>;
-  semester2: FirebaseListObservable<any[]>;
+
   sem1Ref: any;
   sem1List: any;
   loadedsem1List: any;
-  sem2Ref: any;
-  sem2List: any;
-  loadedsem2List: any;
+
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public af: AngularFire) {
+              public navParams: NavParams) {
 
           
-                this.sem1Ref = firebase.database().ref('/faculty/semester1');
-                this.sem2Ref = firebase.database().ref('/faculty/semester2');
+                this.sem1Ref = firebase.database().ref('/faculty/semester1',);
 
 this.sem1Ref.on('value', sem1List => {
-  let countries = [];
+  let items = [];
   sem1List.forEach( sem1 => {
-    countries.push(sem1.val());
+    items.push(sem1.val());
   });
 
-  this.sem1List = countries;
-  this.loadedsem1List = countries;
+  this.sem1List = items;
+  this.loadedsem1List = items;
 });
 
   }
@@ -39,21 +33,16 @@ initializeItems(): void {
   this.sem1List = this.loadedsem1List;
 }
   getItems(searchbar) {
-  // Reset items back to all of the items
   this.initializeItems();
-
-  // set q to the value of the searchbar
   var q = searchbar.srcElement.value;
 
-
-  // if the value is an empty string don't filter the items
   if (!q) {
     return;
   }
 
   this.sem1List = this.sem1List.filter((v) => {
-    if(v.name && q) {
-      if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+    if((v.name && q) || (v.email && q) || (v.course && q)){
+      if ((v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) || (v.email.toLowerCase().indexOf(q.toLowerCase()) > -1) ||(v.course.toLowerCase().indexOf(q.toLowerCase()) > -1)) {
         return true;
       }
       return false;
