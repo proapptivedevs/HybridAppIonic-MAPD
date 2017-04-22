9b136d21bacd98b30c9ee3035cc9d063d65d60ad
Luis Esquivel <laesquivel@hotmail.com>
2017-04-21 22:26:56 -0400

    last filter

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-about',
@@ -10,15 +10,58 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class AboutPage {
   semester1: FirebaseListObservable<any[]>;
   semester2: FirebaseListObservable<any[]>;
-  
+  sem1Ref: any;
+  sem1List: any;
+  loadedsem1List: any;
+  sem2Ref: any;
+  sem2List: any;
+  loadedsem2List: any;
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public af: AngularFire) {
 
-                this.semester1 = af.database.list('/faculty/semester1');
-                this.semester2 = af.database.list('/faculty/semester2');
+          
+                this.sem1Ref = firebase.database().ref('/faculty/semester1');
+                this.sem2Ref = firebase.database().ref('/faculty/semester2');
 
+this.sem1Ref.on('value', sem1List => {
+  let countries = [];
+  sem1List.forEach( sem1 => {
+    countries.push(sem1.val());
+  });
+
+  this.sem1List = countries;
+  this.loadedsem1List = countries;
+});
 
   }
+initializeItems(): void {
+  this.sem1List = this.loadedsem1List;
+}
+  getItems(searchbar) {
+  // Reset items back to all of the items
+  this.initializeItems();
+
+  // set q to the value of the searchbar
+  var q = searchbar.srcElement.value;
+
+
+  // if the value is an empty string don't filter the items
+  if (!q) {
+    return;
+  }
+
+  this.sem1List = this.sem1List.filter((v) => {
+    if(v.name && q) {
+      if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    }
+  });
+
+  console.log(q, this.sem1List.length);
+
+}
 
 }
